6ec1a25d78e382fe7289061ea58eecd41ac84774
IWAMOTO Takashi <hello@iwamot.com>
2008-07-31 21:18:21 +0900

    add tests

--- lib/pinto/model/user.rb
+++ lib/pinto/model/user.rb
@@ -4,7 +4,15 @@ module Pinto
   module Model
     class User
       def self.add(user_name, claimed_id, lang)
+        user_name = user_name.to_s
 
+        if user_name.empty?
+          translator = Pinto::Translate.new(lang)
+          message = translator._('%{param} is required') % {
+            :param => translator._('User name')
+          }
+          raise ArgumentError.new(message)
+        end
       end
     end
   end
--- test/model/user_spec.rb
+++ test/model/user_spec.rb
@@ -2,11 +2,29 @@ $LOAD_PATH << 'lib'
 require 'pinto'
 
 describe Pinto::Model::User, 'when user_name is nil for add method' do
-  it 'should raise error in English when lang is "en"' do
-    lambda {Pinto::Model::User.add(nil, 'dummy', 'en')}.should raise_error
+  it 'should raise English error when lang is "en"' do
+    lambda {
+      Pinto::Model::User.add(nil, 'dummy', 'en')
+    }.should raise_error(ArgumentError, 'User name is required')
   end
 
-  it 'should raise error in Japanese when lang is "ja"' do
-    lambda {Pinto::Model::User.add(nil, 'dummy', 'ja')}.should raise_error
+  it 'should raise Japanese error when lang is "ja"' do
+    lambda {
+      Pinto::Model::User.add(nil, 'dummy', 'ja')
+    }.should raise_error(ArgumentError, 'ユーザー名は必須です')
+  end
+end
+
+describe Pinto::Model::User, 'when user_name is empty for add method' do
+  it 'should raise English error when lang is "en"' do
+    lambda {
+      Pinto::Model::User.add('', 'dummy', 'en')
+    }.should raise_error(ArgumentError, 'User name is required')
+  end
+
+  it 'should raise Japanese error when lang is "ja"' do
+    lambda {
+      Pinto::Model::User.add('', 'dummy', 'ja')
+    }.should raise_error(ArgumentError, 'ユーザー名は必須です')
   end
 end
