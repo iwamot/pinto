9d768d625a918dd0ff416f779e6c12f396467ef3
IWAMOTO Takashi <hello@iwamot.com>
2008-10-29 00:45:29 +0900

    trying Cucumber

--- features/steps/http_steps.rb
+++ features/steps/http_steps.rb
@@ -3,13 +3,52 @@ require 'spec'
 require 'net/http'
 require 'uri'
 
-When /I (\w+) (.+)/ do |method, uri|
-  parsed_uri = URI.parse(uri)
-  http = Net::HTTP.new(parsed_uri.host, parsed_uri.port)
-  @response = http.send(method.downcase, parsed_uri.path)
+Before do
+  @headers = {}
 end
 
-Then /the response status code should be (\d{3})/ do |code|
+Given /^I have set (.+) as a target URI$/ do |uri|
+  @uri = uri
+end
+
+Given /^I have set (\w+) If-Modified-Since header$/ do |condition|
+  if condition == 'modified'
+    @headers['If-Modified-Since'] = 'hogehoge'
+  elsif condition == 'unmodified'
+    @headers['If-Modified-Since'] = 'fugafuga'
+  end
+end
+
+Given /^I have set (\w+) If-None-Match header$/ do |condition|
+  if condition == 'modified'
+    @headers['If-None-Match'] = 'hogehoge'
+  elsif condition == 'unmodified'
+    @headers['If-None-Match'] = 'fugafuga'
+  end
+end
+
+Given /^I have set (\w+) If-Unmodified-Since header$/ do |condition|
+  if condition == 'modified'
+    @headers['If-Unmodified-Since'] = 'hogehoge'
+  elsif condition == 'unmodified'
+    @headers['If-Unodified-Since'] = 'fugafuga'
+  end
+end
+
+Given /^I have set (\w+) If-Match header$/ do |condition|
+  if condition == 'modified'
+    @headers['If-Match'] = 'hogehoge'
+  elsif condition == 'unmodified'
+    @headers['If-Match'] = 'fugafuga'
+  end
+end
+
+When /^I GET$/ do
+  parsed_uri = URI.parse(@uri)
+  http = Net::HTTP.new(parsed_uri.host, parsed_uri.port)
+  @response = http.get(parsed_uri.path, @headers)
+end
+
+Then /^the response status code should be (\d{3})$/ do |code|
   @response.code.to_i.should == code.to_i
 end
-
